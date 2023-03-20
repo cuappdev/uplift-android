b31d96bce2a0d5f6cffaec4451ca337a56643c40
ramzd18 <rbpeddu@gmail.com>
2023-03-19 20:51:53 -0400

    Finished Homecard
    Add Icon overlap feature to HomeCard and updated function to take in gym class as a parameter.

--- app/src/main/java/com/cornellappdev/uplift/ui/components/Home/HomeCard.kt
+++ app/src/main/java/com/cornellappdev/uplift/ui/components/Home/HomeCard.kt
@@ -1,5 +1,4 @@
 package com.cornellappdev.uplift.ui.components.Home
-
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
@@ -11,75 +10,119 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import android.R
-class HomeCard {
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.ui.Alignment.Companion.TopEnd
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import com.cornellappdev.uplift.R
+import com.cornellappdev.uplift.models.Gym
+import com.cornellappdev.uplift.util.montserratFamily
 
-    @Composable
-    fun homeCard(  name: Int) {
-        Card(
-            modifier= Modifier.fillMaxWidth().fillMaxHeight().aspectRatio(2F/1F)
 
-        ) {
-            Column() {
-                Column(modifier = Modifier.weight(3F)) {
-                    Image(
-                        modifier = Modifier.fillMaxWidth(),
-                        painter = painterResource(id = R.drawable.ic_btn_speak_now),
-                        contentDescription = "test",
-                        contentScale= ContentScale.FillWidth
-                    )
-
-                }
-                Column(modifier = Modifier.weight(2F)) {
-                    Row(
-                        modifier = Modifier.padding(5.dp)
-                    ) {
-                        Text(
-                            text = "Helen Newman",
-                            fontSize = 20.sp,
-                        )
-                        Spacer(modifier = Modifier.weight(1F))
+@Composable
+    fun homeCard(gymexample:Gym ) {
+    var paintres = remember  {mutableStateOf(if(gymexample.isFavorite()) R.drawable.ic_star_filled else R.drawable.ic_star) }
+        Box(modifier= Modifier
+            .padding(5.dp)
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .aspectRatio(2F / 1F)){
+            Card(
+                modifier = Modifier
+            ) {
+                Column() {
+                    Column(modifier = Modifier.weight(3F)) {
                         Image(
-                            painter = painterResource(id = R.drawable.ic_menu_add),
-                            contentDescription = "test",
-                        )
-                        Image(
-                            painter = painterResource(id = R.drawable.ic_menu_search),
-                            contentDescription = "test",
+                            modifier = Modifier.fillMaxWidth(),
+                            painter = painterResource(id=R.drawable.court_lines),
+                            contentDescription = "Gym Image",
+                            contentScale = ContentScale.FillWidth
                         )
 
                     }
-                    Row(modifier= Modifier.padding(start=5.dp, end=5.dp)) {
-                        Text(
-                            text = "Open",
-                            fontSize = 12.sp,
-                            color = Color.Green
-                        )
-                        Spacer(modifier = Modifier.width(10.dp))
-                        Text(
-                            text = "Closes at 6 pm",
-                            fontSize = 12.sp,
-                        )
+                    Column(modifier = Modifier.weight(2F)) {
+                        Row(
+                            modifier = Modifier.padding(5.dp)
+                        ) {
+                            Text(
+                                text =gymexample.name,
+                                fontSize = 20.sp,
+                                fontFamily = montserratFamily
 
-                    }
-                    Row(modifier= Modifier.padding(start=5.dp,
-                        end= 5.dp)) {
-                        Text(
-                            text = "Cramped",
-                            fontSize = 12.sp,
-                            color = Color.Red
-                        )
-                        Spacer(modifier = Modifier.width(10.dp))
-                        Text(
-                            text = " 120/140",
-                            fontSize = 12.sp,
-                        )
-                        Spacer(modifier= Modifier.weight(1F))
-                        Text("1.2mi")
+                                )
+                            Spacer(modifier = Modifier.weight(1F))
+                            Image(
+                                painter = painterResource(id = R.drawable.ic_dumbbell),
+                                contentDescription = "Dumbell",
+                            )
+                            Image(
+                                painter = painterResource(id = R.drawable.ic_bowling_pins),
+                                contentDescription = "Bowling Pins",
+                            )
+
+                        }
+                        Row(modifier = Modifier.padding(start = 5.dp, end = 5.dp)) {
+                            Text(
+                                text = "Open",
+                                fontSize = 12.sp,
+                                color = Color.Green,
+                                fontFamily = montserratFamily
+
+                                )
+                            Spacer(modifier = Modifier.width(10.dp))
+                            Text(
+                                text = "Closes at 6 pm",
+                                fontSize = 12.sp,
+                                fontFamily = montserratFamily
+                                )
+
+                        }
+                        Row(
+                            modifier = Modifier.padding(
+                                start = 5.dp,
+                                end = 5.dp
+                            )
+                        ) {
+                            Text(
+                                text = "Cramped",
+                                fontSize = 12.sp,
+                                color = Color.Red,
+                                fontFamily = montserratFamily
+                                )
+                            Spacer(modifier = Modifier.width(10.dp))
+                            Text(
+                                text = " 120/140",
+                                fontSize = 12.sp,
+                                fontFamily = montserratFamily
+                            )
+                            Spacer(modifier = Modifier.weight(1F))
+                            Text(
+                                text="1.2mi",
+                                fontFamily = montserratFamily
+                            )
+
+                        }
                     }
                 }
+            }
+            /**
+             * Changes Icon image to either be filled star or regular star depending on if the user has liked
+             * that particular gym.
+              */
+            IconButton(
+                onClick = {
+                    gymexample.toggleFavorite() },
+                modifier = Modifier
+                    .size(24.dp)
+                    .align(TopEnd),
+            ) {
+                Icon(
+                    painterResource(id=paintres.value),
+                    contentDescription="Star Icon"
+
+                )
             }
         }
     }
 
-}
