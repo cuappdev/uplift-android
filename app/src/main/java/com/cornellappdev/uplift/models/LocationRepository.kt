e85dc8490ec68b3575445a20105ff37a09b71664
thisjustin123 <jjg283@cornell.edu>
2023-11-06 17:36:17 -0500

    Revert failed location redo
    - MutableState should be better here

--- app/src/main/java/com/cornellappdev/uplift/models/LocationRepository.kt
+++ app/src/main/java/com/cornellappdev/uplift/models/LocationRepository.kt
@@ -4,24 +4,25 @@ import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
 
 /**
  * Collection of all location data for the user.
  */
 object LocationRepository {
     private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private val _currentLocation: MutableStateFlow<Location?> = MutableStateFlow(null)
+    private val _currentLocation: MutableState<Location?> = mutableStateOf(null)
 
     /**
-     * Either emits the current user's location, or null if the location has not yet
+     * Either is the current user's location, or null if the location has not yet
      * been initialized.
      * */
-    var currentLocation = _currentLocation.asStateFlow()
+    var currentLocation = (_currentLocation as State<Location?>)
 
     /**
      * Starts updating [currentLocation] to the user's current location.
