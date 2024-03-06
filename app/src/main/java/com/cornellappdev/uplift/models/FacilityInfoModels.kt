ff1b703626edef5d84ceb132c00404c60687e8e0
Corfish123 <80220215+Corfish123@users.noreply.github.com>
2024-03-06 18:24:53 -0500

    Finished equipment backend

--- app/src/main/java/com/cornellappdev/uplift/models/FacilityInfoModels.kt
+++ app/src/main/java/com/cornellappdev/uplift/models/FacilityInfoModels.kt
@@ -1,5 +1,9 @@
 package com.cornellappdev.uplift.models
 
+import com.cornellappdev.uplift.type.AccessibilityType
+import com.cornellappdev.uplift.type.EquipmentType
+import kotlin.collections.ArrayList
+
 /**
  * A facility representing one court at a fitness center.
  */
@@ -41,16 +45,49 @@ data class BowlingInfo(
     val shoeRental: String
 )
 
+/**
+ * Representation type of accessibility there is at the facility
+ */
+//enum class AccessibilityType {
+//    WHEELCHAIR
+//}
+/**
+ * Representation type of Equipment each is
+ */
+//enum class EquipmentType{
+//    CARDIO,
+//    RACKS_AND_BENCHES,
+//    SELECTORIZED,
+//    MULTI_CABLE,
+//    FREE_WEIGHTS,
+//    MISCELLANEOUS,
+//    PLATE_LOADED,
+//}
+
+/** An [EquipmentField] is all of the equipment that exist at a gym facility */
+data class EquipmentField(
+    /** The title of this equipment grouping. (e.g. "Cardio Machines") */
+    val id : String,
+    val accessibility: AccessibilityType?,
+    val name: String,
+    val facilityId: Int,
+    val quantity: Int
+)
+
 /** An [EquipmentGrouping] is a grouping of one or more pieces of gym equipment under a particular
  * category. [UpliftGym] objects may have multiple [EquipmentGrouping]s to specify all the equipment
  * they carry. */
 data class EquipmentGrouping(
-    /** The title of this equipment grouping. (e.g. "Cardio Machines") */
-    val name: String,
-    /** A list of equipment that this grouping offers.
+
+    /** A list of equipment that this grouping offe
+    val equipmentType: EquipmentType,rs.
      *
      * Example: The list (("Treadmills", 5), ("Rowing Machines", 3)) indicates that this grouping has
      * 5 treadmills and 3 rowing machines.
      * */
-    val equipmentList: List<Pair<String, Int>>
+//    val name: String,
+    val equipmentType: EquipmentType,
+//    val equipmentList: List<Pair<String, Int>>
+    val equipmentList: ArrayList<EquipmentField>
+
 )
