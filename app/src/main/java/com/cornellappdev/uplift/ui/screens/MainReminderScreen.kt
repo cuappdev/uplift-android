b5093efdeae39a8e202645f7887538299bd4cd73
vstarzf <vstarzf@gmail.com>
2024-10-24 20:51:02 -0400

    fixes ui details + file formatting

--- app/src/main/java/com/cornellappdev/uplift/ui/screens/MainReminderScreen.kt
+++ app/src/main/java/com/cornellappdev/uplift/ui/screens/MainReminderScreen.kt
@@ -1,26 +1,6 @@
 package com.cornellappdev.uplift.ui.screens
 
-import androidx.compose.animation.Crossfade
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.platform.LocalContext
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavHostController
-import com.cornellappdev.uplift.models.LocationRepository
-import com.cornellappdev.uplift.networking.ApiResponse
-import com.cornellappdev.uplift.ui.screens.subscreens.MainError
-import com.cornellappdev.uplift.ui.screens.subscreens.MainLoaded
-import com.cornellappdev.uplift.ui.screens.subscreens.MainLoading
-import com.cornellappdev.uplift.ui.viewmodels.ClassDetailViewModel
-import com.cornellappdev.uplift.ui.viewmodels.GymDetailViewModel
-import com.cornellappdev.uplift.ui.viewmodels.HomeViewModel
-import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.rememberMultiplePermissionsState
-import com.valentinilk.shimmer.Shimmer
-import kotlinx.coroutines.delay
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -28,15 +8,14 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -45,17 +24,14 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.cornellappdev.uplift.R
-import com.cornellappdev.uplift.util.GRAY00
 import com.cornellappdev.uplift.util.GRAY01
 import com.cornellappdev.uplift.util.LIGHT_GRAY
 import com.cornellappdev.uplift.util.montserratFamily
 import com.google.accompanist.systemuicontroller.SystemUiController
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
-/**
- * The home page of Uplift.
- */
-@OptIn(ExperimentalPermissionsApi::class, ExperimentalMaterial3Api::class)
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainReminderScreen() {
     val systemUiController: SystemUiController = rememberSystemUiController()
@@ -65,12 +41,17 @@ fun MainReminderScreen() {
     systemUiController.isSystemBarsVisible = false
 
     Scaffold(topBar = {
-        TopAppBar(
-            title =  {Text(
-                text = "Reminders",
-                fontSize = 24.sp,
-                fontFamily = montserratFamily,
-                fontWeight = FontWeight.Bold) },
+        CenterAlignedTopAppBar(
+            expandedHeight = 40.dp,
+            title = {
+                Text(
+                    text = "Reminders",
+                    fontSize = 16.sp,
+                    lineHeight = 20.sp,
+                    fontFamily = montserratFamily,
+                    fontWeight = FontWeight.Bold
+                )
+            },
             colors = TopAppBarDefaults.topAppBarColors(containerColor = LIGHT_GRAY),
             navigationIcon = {
                 IconButton(onClick = {}) {
@@ -78,12 +59,10 @@ fun MainReminderScreen() {
                 }
             }
 
-        ) }) {
-        innerPadding ->
+        )
+    }) { innerPadding ->
         Column(modifier = Modifier.padding(innerPadding)) {
             Column(modifier = Modifier.padding(horizontal = 24.dp)) {
-                ReminderListItem("Class Reminders", R.drawable.classes, {})
-                HorizontalDivider(color = GRAY01)
                 ReminderListItem("Workout Reminders", R.drawable.gym_simple, {})
                 HorizontalDivider(color = GRAY01)
                 ReminderListItem("Capacity Reminders", R.drawable.capacities, {})
@@ -94,8 +73,14 @@ fun MainReminderScreen() {
 
 @Composable
 private fun ReminderListItem(txt: String, resourceId: Int, onClick: () -> Unit) {
-    Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.height(72.dp).fillMaxWidth()) {
-        Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .height(72.dp)
+            .fillMaxWidth()
+    ) {
+        Row(horizontalArrangement = Arrangement.spacedBy(8.dp), verticalAlignment = Alignment.CenterVertically) {
             Image(painter = painterResource(resourceId), contentDescription = "")
 
             Text(
